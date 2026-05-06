cc483a19c36412b0ae6cf4ed1544fd85285633c5
0dataDesk <0.datadesk@gmail.com>
2026-05-05 18:18:24 -0600

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
-const { createClient } = supabase
+const { createClient } = window.supabase
 
 const supabaseUrl = 'https://gzawztrjekesklzepatf.supabase.co'
 const supabaseKey = 'sb_publishable_EFa8K18Ho6mENPOv5GsREQ__6FnC1jO'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabaseClient = createClient(supabaseUrl, supabaseKey)
+export { supabaseClient as supabase }
