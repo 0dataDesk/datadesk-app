b86b54db22250552030215fe1a68a67ace93e4e1
0dataDesk <0.datadesk@gmail.com>
2026-05-05 18:15:39 -0600

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,11 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
-import 'dotenv/config'
+const { createClient } = supabase
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_PUBLISHABLE_KEY
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Faltan variables de entorno: SUPABASE_URL o SUPABASE_PUBLISHABLE_KEY')
-}
+const supabaseUrl = 'https://gzawztrjekesklzepatf.supabase.co'
+const supabaseKey = 'sb_publishable_EFa8K18Ho6mENPOv5GsREQ__6FnC1jO'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
