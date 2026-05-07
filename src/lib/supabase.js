5f11ccc55c6193591786900ad8cdcdcba5de127c
0dataDesk <0.datadesk@gmail.com>
2026-05-06 20:47:36 -0600

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = window.supabase
 
 const supabaseUrl = 'https://gzawztrjekesklzepatf.supabase.co'
-const supabaseKey = 'sb_publishable_EFa8K18Ho6mENPOv5GsREQ__6FnC1jO'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6YXd6dHJqZWtlc2tsemVwYXRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NDMwMTQsImV4cCI6MjA5MzQxOTAxNH0.88INicH2fkZ-jsIoNpx9kP6510yel-ADLyExGlDCBX4'
 
 export const supabaseClient = createClient(supabaseUrl, supabaseKey)
 export { supabaseClient as supabase }
