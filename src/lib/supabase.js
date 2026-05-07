a723f22d3c897c6f66b84b23d7177b9d75ba39c8
0dataDesk <0.datadesk@gmail.com>
2026-05-06 21:05:14 -0600

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,13 +1,1 @@
-const { createClient } = window.supabase
-
-const supabaseUrl = 'https://gzawztrjekesklzepatf.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6YXd6dHJqZWtlc2tsemVwYXRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NDMwMTQsImV4cCI6MjA5MzQxOTAxNH0.88INicH2fkZ-jsIoNpx9kP6510yel-ADLyExGlDCBX4'
-
-export const supabaseClient = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: false
-  }
-})
-export { supabaseClient as supabase }
+export const supabase = window._supabase
