abd09508f782651bbe7f08fe3d6a6c61d1fcf826
0dataDesk <0.datadesk@gmail.com>
2026-05-06 21:11:14 -0600

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,7 @@
-export const supabase = window._supabase
+const { createClient } = window.supabase
+
+export const supabase = createClient(
+  'https://gzawztrjekesklzepatf.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6YXd6dHJqZWtlc2tsemVwYXRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NDMwMTQsImV4cCI6MjA5MzQxOTAxNH0.88INicH2fkZ-jsIoNpx9kP6510yel-ADLyExGlDCBX4',
+  { auth: { persistSession: true, autoRefreshToken: true, storageKey: 'datadesk-auth' } }
+)
